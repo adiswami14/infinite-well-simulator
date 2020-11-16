2a5d3107afc9b290c537bb5d8213039ef9d5ea5e
adiswami14 <adithya1431@gmail.com>
2020-11-15 19:32:14 -0500

    Get rid of overloaded constructor in Particle struct

--- src/core/particle.cc
+++ src/core/particle.cc
@@ -7,12 +7,4 @@ Particle::Particle() {
     energy_state_ = 1;
 }
 
-Particle::Particle(double mass, size_t energy_state) {
-    if(mass <=0 || energy_state<=0) {
-        throw std::invalid_argument("Mass and/or energy state can only be positive values!");
-    }
-    mass_ = mass;
-    energy_state_ = energy_state;
-}
-
 }
